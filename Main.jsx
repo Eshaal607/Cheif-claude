a41a76c9110d93105b87b868db8cf0c3df788430
Eshaal607 <eshaalbaloch141@gmail.com>
2026-09-11 16:21:51 +0500

    did the ingredient list

--- Main.jsx
+++ Main.jsx
@@ -1,13 +1,31 @@
 export default function Main() {
+    const ingredients = ["chicken", "soup", "Tomatoes"]
+
+    const ingriedientlist = ingredients.map(ingrient => (
+        <li key={ingrient}>{ingrient}</li>
+    ))
+
+    function onsubmit(event){
+        event.preventDefault()
+        console.log("Form submitted")
+        const formdata = new FormData(event.currentTarget)
+        const newingredeint = formdata.get("ingredient")
+        ingredients.push(newingredeint)
+        console.log(ingredients)
+    }
     return (
         <main>
-            <form className="addIngForm">
+            <form className="addIngForm" onSubmit={onsubmit}>
                 <input 
                 aria-label="Add ingredient"
                 type="text"
-                placeholder="e.g Oregano" />
+                placeholder="e.g Oregano"
+                name="ingredient" />
                 <button>+ Add ingredient</button>
             </form>
+            <ul>
+                {ingriedientlist}
+            </ul>
         </main>
     )
 }
